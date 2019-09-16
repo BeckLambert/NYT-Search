d2dac213abf07c2c17cb9ec824307042a6f18588
lambertco <blambertco@gmail.com>
2019-09-16 13:30:58 -0600

    begin and end dates added to url

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-var apikey = "FZKSZP72n3AR2H39MbCcX0aN2kQXvlqu";
     var keyword = "goldfish";
-    //var url = "https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=" + apikey;
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keyword + "&api-key=" + apikey;
+    var beginDate = "";
+    var endDate = "";
+    var apikey = "FZKSZP72n3AR2H39MbCcX0aN2kQXvlqu";
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&q=begin_date&end_date" + keyword + "&api-key=" + apikey;
+    
+    
     $.ajax({
         url: url,
         method: "GET"
     }).then(function (response) {
         console.log(response);
     });
+
