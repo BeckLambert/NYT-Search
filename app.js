303da6d7dc92fb84c1fce457bb465d7fff2ef7d3
Nick Cooper <charliecoop@gmail.com>
2019-09-16 14:25:50 -0600

    append headlines

--- app.js
+++ app.js
@@ -1,47 +1,45 @@
-    var keyword = "goldfish";
-    var beginDate = $("#start-year").val();
-    var endDate = $("#end-year").val();
-    var begin;
-    var end;
-    var updatePage;
-    
-    if(beginDate !== ""){
-        begin = "&begin_date=" + beginDate;
-    } else {
-        begin = "";
-    };
-    
-    if(endDate !== ""){
-        end = "&end_date=" + endDate;
-    } else {
-        end = "";
-    };
-    
-    var apikey = "qukgqMyzFb2sUd4XG8ajQcrhAmlqkp1W";
-    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&q=" + keyword + "&api-key=" + apikey + begin + end;
-    console.log(url);
-    function clear() {
-        $("#article-section").empty();
-      }
-    
-      $("#run-search").on("click", function(event) {
-        event.preventDefault();
-        clear();
-        
-        $.ajax({
-          url: url,
-          method: "GET"
-        }).then(function(response){
-            console.log(response);
-            var results = response.data
-            for (var i = 0; i < results.length; i++) {
-                var articleDiv = $('<div>');
-                animalImage.attr("src", results[i].images.fixed_height.url);
-                animalDiv.append(p, animalImage);
-                $('#gifs-appear-here').prepend(animalDiv);
-        }).then(function (response){
-            console.log(response);
-        });
-      });
-    
-      $("#clear-all").on("click", clear);
+var keyword = "goldfish";
+var beginDate = $("#start-year").val();
+var endDate = $("#end-year").val();
+var begin;
+var end;
+var updatePage;
+
+if (beginDate !== "") {
+    begin = "&begin_date=" + beginDate;
+} else {
+    begin = "";
+};
+
+if (endDate !== "") {
+    end = "&end_date=" + endDate;
+} else {
+    end = "";
+};
+
+var apikey = "qukgqMyzFb2sUd4XG8ajQcrhAmlqkp1W";
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&q=" + keyword + "&api-key=" + apikey + begin + end;
+console.log(url);
+function clear() {
+    $("#article-section").empty();
+}
+
+$("#run-search").on("click", function (event) {
+    event.preventDefault();
+    clear();
+
+    $.ajax({
+        url: url,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        var results = response.response.docs;
+        for (var i = 0; i < results.length; i++) {
+            var articleDiv = $('<div>');
+           
+            $('#article-section').append(results[i].headline.main);
+        }
+    });
+});
+
+$("#clear-all").on("click", clear);
